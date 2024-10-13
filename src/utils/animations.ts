643e4af6d57dd49495bac0627f48726542cd1b6b
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-10-13 17:14:54 +0200

    change image animation

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -3,5 +3,17 @@ import { gsap } from "gsap";
 export const slowEase = "slow(0.3,0.3,false)";
 
 export const slideIn = (selector: string, delay: number = 0) => {
-  gsap.fromTo(selector, { y: 75 }, { y: 0, duration: 0.7, ease: slowEase, delay });
+  gsap.fromTo(
+    selector,
+    { y: 75 },
+    { y: 0, duration: 0.7, ease: slowEase, delay }
+  );
+};
+
+export const slideInWithFade = (selector: string, delay: number = 0) => {
+  gsap.fromTo(
+    selector,
+    { y: 75, opacity: 0 },
+    { y: 0, opacity: 1, duration: 0.7, ease: slowEase, delay }
+  );
 };
