88e36cfdfdca5693412cf26692f6011bedb82d6d
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-10-14 18:39:23 +0200

    added clients section

---
+++ src/constants/const.ts
@@ -0,0 +1,99 @@
+export const workplaces = [
+  {
+    time: "2023 - Present",
+    name: "NativeState",
+    role: "Design Director",
+    tag: "Co-Founder",
+  },
+  {
+    time: "2017 - Present",
+    name: "OH.STUDIO",
+    role: "Freelance Designer",
+    tag: "Director",
+  },
+  {
+    time: "2021 - Present",
+    name: "Friendly Studio",
+    role: "Lead Product Designer",
+    tag: "Freelance",
+  },
+  {
+    time: "2023 - Present",
+    name: "NativeState",
+    role: "Design Director",
+    tag: "Co-Founder",
+  },
+  {
+    time: "2017 - Present",
+    name: "OH.STUDIO",
+    role: "Freelance Designer",
+    tag: "Director",
+  },
+  {
+    time: "2021 - Present",
+    name: "Friendly Studio",
+    role: "Lead Product Designer",
+    tag: "Freelance",
+  },
+  {
+    time: "2023 - Present",
+    name: "NativeState",
+    role: "Design Director",
+    tag: "Co-Founder",
+  },
+  {
+    time: "2017 - Present",
+    name: "OH.STUDIO",
+    role: "Freelance Designer",
+    tag: "Director",
+  },
+  {
+    time: "2021 - Present",
+    name: "Friendly Studio",
+    role: "Lead Product Designer",
+    tag: "Freelance",
+  },
+  {
+    time: "2023 - Present",
+    name: "NativeState",
+    role: "Design Director",
+    tag: "Co-Founder",
+  },
+  {
+    time: "2017 - Present",
+    name: "OH.STUDIO",
+    role: "Freelance Designer",
+    tag: "Director",
+  },
+  {
+    time: "2021 - Present",
+    name: "Friendly Studio",
+    role: "Lead Product Designer",
+    tag: "Freelance",
+  },
+];
+
+export const clients = [
+  { name: "Eurosport", imagePath: "/src/assets/images/clients/eurosport.png" },
+  { name: "R/GA", imagePath: "/src/assets/images/clients/rga.png" },
+  { name: "Nike", imagePath: "/src/assets/images/clients/nike.png" },
+  { name: "Olympics", imagePath: "/src/assets/images/clients/olympics.png" },
+  { name: "schuh", imagePath: "/src/assets/images/clients/schuh.png" },
+  { name: "Google", imagePath: "/src/assets/images/clients/google.png" },
+  { name: "IBM", imagePath: "/src/assets/images/clients/ibm.png" },
+  { name: "Koto", imagePath: "/src/assets/images/clients/ko.png" },
+  { name: "Vercel", imagePath: "/src/assets/images/clients/vercel.png" },
+  { name: "MuscleGun", imagePath: "/src/assets/images/clients/musclegun.png" },
+  { name: "Akqa", imagePath: "/src/assets/images/clients/akqa.png" },
+  {
+    name: "World Gold Council",
+    imagePath: "/src/assets/images/clients/wgc.png",
+  },
+  { name: "5", imagePath: "/src/assets/images/clients/5.png" },
+  {
+    name: "Correlated",
+    imagePath: "/src/assets/images/clients/correlated.png",
+  },
+  { name: "here", imagePath: "/src/assets/images/clients/here-white.png" },
+  { name: "ZeroLight", imagePath: "/src/assets/images/clients/zerolight.png" },
+];
